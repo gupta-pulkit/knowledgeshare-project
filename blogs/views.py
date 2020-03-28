717166ecb38057ae81222e345b599b5daa2f19ed
unknown <guptapulkit48@gmail.com>
2020-03-28 22:21:17 +0530

    Added Blog Model

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from .models import Blog
 
 def home(request):
     return render(request, 'blogs/home.html')
 
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk = blog_id)
+    return render(request, 'blogs/detail.html', {'blog': blog})
+
+@login_required(login_url = "/accounts/signup")
 def create(request):
-    return render(request, 'blogs/create.html')
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.FILES['image']:
+            blog = Blog()
+            blog.title = request.POST['title']
+            blog.body = request.POST['body']
+            blog.image = request.FILES['image']
+            blog.pub_date = timezone.datetime.now()
+            blog.author = request.user
+            blog.save()
+            return redirect('/blogs/'+str(blog.id))
+        else:
+            return render(request, 'blogs/create.html', {'error': "All fields are required"})
+    else:
+        return render(request, 'blogs/create.html')
