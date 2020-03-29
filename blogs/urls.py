d0f6ae0982c025c8a0483ce6d55ed9e44875b967
unknown <guptapulkit48@gmail.com>
2020-03-29 10:48:33 +0530

    Category Navbar up and running

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create, name = 'create'),
+    path('category/<int:blog_id>/', views.detail, name = 'detail'),
     path('<str:cat>/', views.category, name = 'category'),
-    path('<int:blog_id>/', views.detail, name = 'detail'),
     path('<int:blog_id>/like', views.like_blog, name = 'like_blog'),
 ]
