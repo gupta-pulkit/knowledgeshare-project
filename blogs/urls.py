2e6e9cc3bb6c74d652ecccf66d3189e76c3689a8
unknown <guptapulkit48@gmail.com>
2020-03-29 15:54:52 +0530

    Added the feature to view your published posts and delete them/

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create, name = 'create'),
-    path('category/<int:blog_id>/', views.detail, name = 'detail'),
-    path('<str:cat>/', views.category, name = 'category'),
+    path('detail/<int:blog_id>/', views.detail, name = 'detail'),
+    path('category/<str:cat>/', views.category, name = 'category'),
     path('<int:blog_id>/like', views.like_blog, name = 'like_blog'),
+    path('myposts/', views.myposts, name = 'myposts'),
+    path('delete/<int:blog_id>/', views.delete, name = 'delete'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseNotFound
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from .models import Blog
@@ -16,6 +17,24 @@ def detail(request, blog_id):
     return render(request, 'blogs/detail.html', {'blog': blog})
 
 @login_required(login_url = "/accounts/signup")
+def delete(request, blog_id):
+    if request.method == 'POST':
+        if request.user == Blog.objects.get(id = blog_id).author:
+            instance = Blog.objects.get(id = blog_id)
+            instance.delete()
+            return redirect('myposts')
+        else:
+            return HttpResponseNotFound("You are not authorized to delete this post!")
+    else:
+        blogs = Blog.objects.filter(author = request.user)
+        return render(request, 'blogs/myposts.html', {'blogs': blogs})
+
+@login_required(login_url = "/accounts/signup")
+def myposts(request):
+    blogs = Blog.objects.filter(author = request.user)
+    return render(request, 'blogs/myposts.html', {'blogs': blogs})
+
+@login_required(login_url = "/accounts/signup")
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['body'] and request.FILES['image'] and request.POST['category']:
@@ -27,7 +46,7 @@ def create(request):
             blog.category = request.POST['category']
             blog.author = request.user
             blog.save()
-            return redirect('/blogs/'+str(blog.id))
+            return redirect('/blogs/detail/'+str(blog.id))
         else:
             return render(request, 'blogs/create.html', {'error': "All fields are required"})
     else:
@@ -39,4 +58,4 @@ def like_blog(request, blog_id):
         blog = get_object_or_404(Blog, pk = blog_id)
         blog.likes_total += 1
         blog.save()
-        return redirect('/blogs/'+str(blog.id))
+        return redirect('/blogs/detail/'+str(blog.id))
